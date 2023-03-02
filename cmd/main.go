1d431c6e8ba1875d3474b3a0b1a1d3c12f5eb70f
trevor-arthur <trevor.spaniola@protonmail.com>
2023-03-02 11:58:03 -0700

    reformatting files for lib

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,50 @@
 package main
 
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+/*
+taslib is Trevor Arthur's personal Golang package/library to make life easier.
+https://github.com/trevor-arthur/taslib
+*/
+
+// Working with CSVs:
+
+/*
+WriteToCSV is a function that writes data to a .csv file. It requires the desired output file name,
+csv headers, number of rows/length of entries, and the data to be stored in each row.
+*/
+func WriteToCSV(outputFile string, headers []string, rowCount int, rowData [][]string) {
+
+	// Create .csv file
+	csvFile, err := os.Create(outputFile)
+	if err != nil {
+		log.Fatalln("[!] Error creating file:", err)
+	}
+
+	csvWriter := csv.NewWriter(csvFile)
+
+	var bodyData [][]string
+	var headerData [][]string
+
+	// Write headers as first line in .csv
+	headerData = append(headerData, headers)
+	csvWriter.WriteAll(headerData)
+
+	// Write row for as many rows as specified in rowCount param
+	for i := 0; i < rowCount; i++ {
+		// Get the row from the array to the .csv file
+		row := rowData[i]
+		bodyData = append(bodyData, row)
+	}
+
+	// Write to CSV
+	csvWriter.WriteAll(bodyData)
+
+}
+
 func main() {
 }
